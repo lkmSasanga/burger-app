98ca33f0cfa15b2e7258ec7e962105944f065a72
lkmSasanga <lkmsasanga@gmail.com>
2021-01-12 13:14:45 +0530

    backdrop added

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -33,7 +33,7 @@ class BurgerIngredient extends Component {
                 ingredient = <div className={classes.Salad}></div>;
                 break;
             default:
-                ingredient: null;
+                // ingredient: null;
         }
 
         return ingredient;
